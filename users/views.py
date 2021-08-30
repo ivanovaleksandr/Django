9e329a08ab877b2a96ab211ad030f710dee664a7
Александр <inter06@.mail.ru>
2021-08-30 21:49:41 +0300

    Урок 6

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth, messages
 from django.urls import reverse
 from baskets.models import Basket
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -37,7 +38,7 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
     user = request.user
     if request.method == 'POST':
